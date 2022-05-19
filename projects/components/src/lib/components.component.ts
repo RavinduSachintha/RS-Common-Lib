2ef09ab01c8ea4d7280ca08f09acf191f899b00b
RavinduSachintha <ravindusachintha53@gmail.com>
2022-05-19 23:53:49 +0530

    init commit

---
+++ projects/components/src/lib/components.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'rs-components-components',
+  template: `
+    <p>
+      components works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class ComponentsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
