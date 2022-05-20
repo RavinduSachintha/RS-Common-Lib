8c0853a351ff0f4a7a799423a42f819bd1a2bb14
RavinduSachintha <ravindusachintha53@gmail.com>
2022-05-20 07:31:40 +0530

    publishing demo project

---
+++ publisher.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+if [ "$1" == "components" ]; then
+    echo "########### Angular Build - Components Library ###########"
+    echo
+    npm run build $1 --prod
+
+    echo "########### copy npmrc file to dist directory ###########"
+    echo
+    cp ./.npmrc ./dist/components
+
+    echo "########### cd to dist directory ###########"
+    echo
+    cd ./dist/components
+
+    echo "########### publish to NPM registry ###########"
+    echo
+    npm publish
+
+    echo "########### delete npmrc from dist directory ###########"
+    echo
+    rm ./.npmrc
+
+    echo "########### everthing sucess ###########"
+fi
+
+if [ "$1" == "demo" ]; then
+    echo "########### Angular Build - Components Library ###########"
+    echo
+    npm run build $1 --prod
+
+    echo "########### git commit & push ###########"
+    echo
+    git add .
+    git commit -m "publishing demo project"
+    git push origin main
+
+    echo "########### everthing sucess ###########"
+fi
